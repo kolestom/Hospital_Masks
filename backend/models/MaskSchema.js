d05a2cb05cc2c299b562adb4043cd6c713403dab
kolestom <88550454+kolestom@users.noreply.github.com>
2023-02-14 18:01:40 +0100

    MaskSchema changed gitignore added to root

--- backend/models/MaskSchema.js
+++ backend/models/MaskSchema.js
@@ -1,8 +1,11 @@
 const mongoose = require('mongoose');
 
 const MaskSchema = new mongoose.Schema({
+    productID: {type: Number, required: true},
+    name: {type: String, required: true},
     quantity: { type: Number, required: true },
-    price: { type: Number, required: true }
+    pricePerPiece: { type: Number, required: true },
+    currency: {type: String, required: true}
 });
 
 module.exports = mongoose.model('Mask', MaskSchema);
